b6127766380cefd61469408a057240991d7f7d91
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-02-10 23:07:36 +0530

    added education cards

--- script/script.js
+++ script/script.js
@@ -7,17 +7,5 @@ var typed = new Typed('#element', {
 
 
 
-$(window).on('scroll', function() {
-    $('.vertical-scrollable-timeline li').each(function(index, elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
 
-        if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
-            $(elem).addClass('active');
-        } else {
-            $(elem).removeClass('active');
-        }
-    });
-});
+
